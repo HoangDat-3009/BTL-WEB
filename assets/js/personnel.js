a8edffc2742da8dd0c5a389c0c6244d7e76a261c
HoangDat-3009 <119090997+HoangDat-3009@users.noreply.github.com>
2023-10-26 23:01:22 +0700

    Update personnel.js

--- assets/js/personnel.js
+++ assets/js/personnel.js
@@ -2,7 +2,7 @@
 
 // item page
 
-// Hiển thị dữ liệu ban đầu
+// hien thi du lieu khi vao trang
 document.addEventListener("DOMContentLoaded", function () {
   displayData();
   updatePageButtons();
@@ -18,17 +18,17 @@ function displayData() {
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = Math.min(startIndex + itemsPerPage, numRows);
 
-  // Ẩn tất cả các hàng trước khi hiển thị lại
+  // an cac hang
   for (let i = 0; i < numRows; i++) {
     rows[i].style.display = "none";
   }
 
-  // Hiển thị các hàng ở trang hiện tại
+  // hien thi cac hang o trang hien tai
   for (let i = startIndex; i < endIndex; i++) {
     rows[i].style.display = "";
   }
 }
-// Hàm cập nhật nút trang cụ thể
+// ham cap nhap nut trang
 function updatePageButtons() {
   const dataTable = document.getElementById("data-table");
   const rows = dataTable.querySelectorAll("tr");
@@ -67,14 +67,14 @@ function updatePageButtons() {
   }
 }
 
-// Hàm xử lý khi nhấn nút "Trang đầu"
+// nut trang dau
 function firstPage() {
   currentPage = 1;
   displayData();
   updatePageButtons();
 }
 
-// Hàm xử lý khi nhấn nút "Trang cuối"
+// nut trang cuoi
 function lastPage() {
   const dataTable = document.getElementById("data-table");
   const rows = dataTable.querySelectorAll("tr");
@@ -84,7 +84,7 @@ function lastPage() {
   displayData();
   updatePageButtons();
 }
-// Hàm xử lý khi nhấn nút "Trang trước"
+// nut trang truoc
 function previousPage() {
   if (currentPage > 1) {
     currentPage -= 1;
@@ -93,7 +93,7 @@ function previousPage() {
   }
 }
 
-// Hàm xử lý khi nhấn nút "Trang sau"
+// trang sau
 function nextPage() {
   const dataTable = document.getElementById("data-table");
   const rows = dataTable.querySelectorAll("tr");
@@ -106,7 +106,7 @@ function nextPage() {
     updatePageButtons();
   }
 }
-// Kết nối các nút với các hàm xử lý
+// ket noi nut voi ham
 document.getElementById("first-page").addEventListener("click", firstPage);
 document
   .getElementById("previous-page")
@@ -159,35 +159,35 @@ function sortByName() {
   var table = document.getElementById("informationPersonnel");
   var tbody = table.getElementsByTagName("tbody")[0];
 
-  // Lấy các hàng và chuyển chúng thành một mảng
+  // lay hang va chuyen no thanh mang
   var rows = Array.from(tbody.getElementsByTagName("tr"));
 
-  // Sắp xếp các hàng dựa trên nội dung của ô đầu tiên (tên)
+  // sap xep noi dung dua tren o ten
   rows.sort(function (a, b) {
     var nameA = a.getElementsByTagName("td")[2].textContent;
     var nameB = b.getElementsByTagName("td")[2].textContent;
     return nameA.localeCompare(nameB);
   });
 
-  // Xóa phần thân của bảng
+  // xoa phan than bang
   while (tbody.firstChild) {
     tbody.removeChild(tbody.firstChild);
   }
 
-  // Thêm các hàng đã sắp xếp trở lại vào bảng
+  // them hang da sap xep vao
   rows.forEach(function (row) {
     tbody.appendChild(row);
   });
   // danh so thu tu tu dong
-  // Lấy danh sách tất cả các hàng (tr) trong bảng
+  //lay danh sach cac so tu bang
   var row = document.querySelectorAll("#data-table tr");
 
-  // Lặp qua từng hàng
+  // lap qua tung hang
   for (var i = 0; i < row.length; i++) {
     // Lấy thẻ td đầu tiên trong hàng hiện tại
     var firstCell = row[i].querySelector("td");
 
-    // Gán số thứ tự cho thẻ td đầu tiên
+    // gan so tu dong cho o dau 
     firstCell.textContent = i + 1;
   }
   isSortByNameClicked = false;
@@ -203,36 +203,36 @@ function sortByDate() {
   isSortByNameClicked = false;
   isSortByDateClicked = true;
   
-  // Lấy bảng của bạn
+  // lay bang
   var table = document.getElementById("data-table");
 
-  // Lấy tất cả các hàng trong bảng của bạn
+  // lay cac hang trong mang
   var rows = table.rows;
 
-  // Chuyển NodeList thành Mảng
-  var arrayOfRows = Array.prototype.slice.call(rows, 1); // Loại trừ hàng tiêu đề
+  // chuyen thanh mang
+  var arrayOfRows = Array.prototype.slice.call(rows, 1); 
 
-  // Sắp xếp mảng các hàng dựa trên ngày trong cột đầu tiên
+  // sap xep mang cac hang dua tren cot ngay tham gia
   arrayOfRows.sort(function (rowA, rowB) {
     var dateA = rowA.cells[7].innerHTML.split("/").reverse().join("-");
     var dateB = rowB.cells[7].innerHTML.split("/").reverse().join("-");
     return new Date(dateA) - new Date(dateB);
   });
 
-  // Thêm từng hàng vào bảng
+  // them tung hang vao mang
   for (var i = 0; i < arrayOfRows.length; i++) {
     table.appendChild(arrayOfRows[i]);
   }
   // danh so thu tu tu dong
-  // Lấy danh sách tất cả các hàng (tr) trong bảng
+  // lay cac hang trong bang
   var row = document.querySelectorAll("#data-table tr");
 
-  // Lặp qua từng hàng
+  // lap qua cac hang
   for (var i = 0; i < row.length; i++) {
     // Lấy thẻ td đầu tiên trong hàng hiện tại
     var firstCell = row[i].querySelector("td");
 
-    // Gán số thứ tự cho thẻ td đầu tiên
+    // gan so thu tu tu dong cho the dau tien
     firstCell.textContent = i + 1;
   }
   isSortByDateClicked = false;
