8149e8627d7bd99ac5af0e512f99b3691ddf20d4
2dawng laptop <dn173746@gmail.com>
2023-10-26 09:46:34 +0700

    commit

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,12 +1,14 @@
 google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawChart2);
 
 addEventListener("resize", (event) => {
   drawChart();
+  drawChart2();
 });
 function drawChart() {
   var data = google.visualization.arrayToDataTable([
-    ["Month", "Lợi nhuận"],
+    ["Tháng", "Lợi nhuận"],
     [1, 10000000],
     [2, 15000000],
     [3, 20000000],
@@ -54,7 +56,7 @@ function drawChart() {
     width: "100%",
     chartArea: { width: "97%", height: "80%" },
     backgroundColor: { fill: "transparent" },
-    title: "Lợi nhuận (theo tháng)",
+    title: "Lợi nhuận (tháng)",
     titleTextStyle: {
       fontSize: 23,
     },
@@ -66,6 +68,53 @@ function drawChart() {
 
   var chart = new google.visualization.LineChart(
     document.getElementById("chart_div")
+  );
+  chart.draw(data, options);
+}
+function drawChart2() {
+  var data = google.visualization.arrayToDataTable([
+    ["Thứ", "Đơn"],
+    ["T.2", 16],
+    ["T.3", 23],
+    ["T.4", 54],
+    ["T.5", 47],
+    ["T.6", 39],
+    ["T.7", 30],
+    ["CN", 21],
+  ]);
+
+
+
+
+  var options = {
+    hAxis: {
+     
+      gridlines: {
+        color: "none",
+      },
+    },
+    vAxis: {
+      gridlines: {
+        color: "none",
+      },
+    },
+    legend: "none",
+    height: "100%",
+    width: "100%",
+    chartArea: { width: "100%", height: "80%" },
+    backgroundColor: { fill: "transparent" },
+    title: "Đơn hàng đã giao",
+    titleTextStyle: {
+      fontSize: 23,
+    },
+    curveType: 'function',
+    
+
+    
+  };
+
+  var chart = new google.visualization.ColumnChart(
+    document.getElementById("chart_div2")
   );
   chart.draw(data, options);
 }
