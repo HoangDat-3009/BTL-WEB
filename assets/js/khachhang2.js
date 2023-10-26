c42ab97f085aba932baf7268f603d924d57ff64d
2dawng laptop <dn173746@gmail.com>
2023-10-26 13:14:37 +0700

    profile change

--- assets/js/khachhang2.js
+++
@@ -1,6 +0,0 @@
-window.alert("hihi");
-var show_quantity = document.getElementById("show_quantity");
-show_quantity.onchange = function(){
-    let a= show_quantity.options[show_quantity.selectedIndex].value;
-    document.write(a);
-}
